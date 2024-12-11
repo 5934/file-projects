b59c9f4a4d9d9add1531afc9d204138cf930ce5d
Mufty Sped <muftysped@gmail.com>
2024-12-11 13:15:01 +0100

    Add files via upload

---
+++ pspractice.js
@@ -0,0 +1,11 @@
+function myproject() {
+    document.getElementById("demo").innerHTML ="content is now changed! congratulations!";
+}
+
+document.getElementById("demo").innerHTML = 5 + 6;
+
+document.write(5 + 6);
+
+alert(5 + 6);
+
+console. log(5 + 6);
